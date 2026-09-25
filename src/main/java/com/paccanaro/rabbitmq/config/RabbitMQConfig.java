ae7eb6ff29bb84c4589398854d1824690367aa5e
pacsss <paccanaro18@gmail.com>
2026-09-25 14:34:21 -0300

    feat: adiciona configuracao do RabbitMQ (queue, exchange, binding)

---
+++ src/main/java/com/paccanaro/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.paccanaro.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_PAGAMENTOS = "pagamentos.queue";
+    public static final String EXCHANGE_PAGAMENTOS = "pagamentos.exchange";
+    public static final String ROUTING_KEY_PAGAMENTOS = "pagamentos.processar";
+
+    @Bean
+    public Queue filaPagamentos() {
+        return new Queue(QUEUE_PAGAMENTOS, true);
+    }
+
+    @Bean
+    public DirectExchange exchangePagamentos() {
+        return new DirectExchange(EXCHANGE_PAGAMENTOS);
+    }
+
+    @Bean
+    public Binding bindingPagamentos(Queue filaPagamentos, DirectExchange exchangePagamentos) {
+        return BindingBuilder
+                .bind(filaPagamentos)
+                .to(exchangePagamentos)
+                .with(ROUTING_KEY_PAGAMENTOS);
+    }
+}
