e664ba0118f7641dd7b2384bd96b02473dc0f044
pacsss <paccanaro18@gmail.com>
2026-09-25 14:47:29 -0300

    fix: adiciona conversor JSON para evitar erro de deserializacao

--- src/main/java/com/paccanaro/rabbitmq/config/RabbitMQConfig.java
+++ src/main/java/com/paccanaro/rabbitmq/config/RabbitMQConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,5 +32,10 @@ public class RabbitMQConfig {
                 .bind(filaPagamentos)
                 .to(exchangePagamentos)
                 .with(ROUTING_KEY_PAGAMENTOS);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
